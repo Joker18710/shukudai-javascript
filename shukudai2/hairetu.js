31dbc1e3ad294c4a909226dd6544ce2db322bba4
Joker18710 <146701660+Joker18710@users.noreply.github.com>
2023-10-24 15:26:40 +0900

    Add files via upload

---
+++ shukudai2/hairetu.js
@@ -0,0 +1,14 @@
+const date=[
+  {id:1, Department:'秘書室', Adress:'代表', phone:'092-643-3000' },
+  {id:2, Department:'総務部', Adress:'部長室', phone:'093-643-3020' },
+  {id:3, Department:'総務部', Adress:'次長室', phone:'092-643-3022' },
+  {id:4, Department:'課長部', Adress:'課長席', phone:'092-643-3035' },
+  {id:5, Department:'人事課', Adress:'総務係', phone:'092-643-3036' },
+  {id:6, Department:'人事課', Adress:'人事第一係', phone:'092-643-3038' },
+  {id:7, Department:'人事課', Adress:'人事第二係', phone:'092-643-3037' },
+  {id:8, Department:'人事課', Adress:'給与係', phone:'092-643-3040' },
+  {id:9, Department:'人事課', Adress:'別室', phone:'092-643-3042' },
+]
+for(let id=0; id<date.length; id++){
+  console.log(date[id])
+}
